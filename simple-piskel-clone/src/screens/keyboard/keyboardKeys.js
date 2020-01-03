7b775e822a57f14cd2096a58aca614b9f42cbe87
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-01-03 22:29:12 +0300

    refactor: keyboard shortcuts

--- simple-piskel-clone/src/screens/keyboard/keyboardKeys.js
+++ simple-piskel-clone/src/screens/keyboard/keyboardKeys.js
@@ -12,76 +12,92 @@ import { addFrame } from '../frames/frames';
 const startKeyMap = {
   hotkeyFillBucket: 'b',
   hotkeyPencil: 'p',
-  hotkeyEaser: 'e',
+  hotkeyEraser: 'e',
   hotkeyChooseColor: 'c',
-
+  hotkeyAddFrame: 'a',
   hotkeySave: 's',
   hotkeyLoad: 'l',
   hotkeyFullScreen: 'f',
   hotkeyGif: 'g',
+  hotkeyRes1: '1',
+  hotkeyRes2: '2',
+  hotkeyRes3: '3',
 };
 
-document.getElementById('hotkeyFillBucket').value = startKeyMap.hotkeyFillBucket;
-document.getElementById('hotkeyPencil').value = startKeyMap.hotkeyPencil;
-document.getElementById('hotkeyEaser').value = startKeyMap.hotkeyEaser;
-document.getElementById('hotkeyChooseColor').value = startKeyMap.hotkeyChooseColor;
+const hotkeyFillBucket = document.getElementById('hotkeyFillBucket');
+const hotkeyPencil = document.getElementById('hotkeyPencil');
+const hotkeyEraser = document.getElementById('hotkeyEraser');
+const hotkeyChooseColor = document.getElementById('hotkeyChooseColor');
+const hotkeyAddFrame = document.getElementById('hotkeyAddFrame');
+const hotkeySave = document.getElementById('hotkeySave');
+const hotkeyLoad = document.getElementById('hotkeyLoad');
+const hotkeyFullScreen = document.getElementById('hotkeyFullScreen');
+const hotkeyGif = document.getElementById('hotkeyGif');
+const hotkeyRes1 = document.getElementById('hotkeyRes1');
+const hotkeyRes2 = document.getElementById('hotkeyRes2');
+const hotkeyRes3 = document.getElementById('hotkeyRes3');
 
-document.getElementById('hotkeySave').value = startKeyMap.hotkeySave;
-document.getElementById('hotkeyLoad').value = startKeyMap.hotkeyLoad;
-document.getElementById('hotkeyFullScreen').value = startKeyMap.hotkeyFullScreen;
-document.getElementById('hotkeyGif').value = startKeyMap.hotkeyGif;
+hotkeyFillBucket.value = startKeyMap.hotkeyFillBucket;
+hotkeyPencil.value = startKeyMap.hotkeyPencil;
+hotkeyEraser.value = startKeyMap.hotkeyEraser;
+hotkeyChooseColor.value = startKeyMap.hotkeyChooseColor;
+hotkeyAddFrame.value = startKeyMap.hotkeyAddFrame;
+hotkeySave.value = startKeyMap.hotkeySave;
+hotkeyLoad.value = startKeyMap.hotkeyLoad;
+hotkeyFullScreen.value = startKeyMap.hotkeyFullScreen;
+hotkeyGif.value = startKeyMap.hotkeyGif;
+hotkeyRes1.value = startKeyMap.hotkeyRes1;
+hotkeyRes2.value = startKeyMap.hotkeyRes2;
+hotkeyRes3.value = startKeyMap.hotkeyRes3;
 
-// const dict = {
-//   hotkeyFillBucket:
-// };
 
 const keysActivation = function keysActivation(event) {
   switch (event.key) {
-    case document.getElementById('hotkeyFillBucket').value:
+    case hotkeyFillBucket.value:
       fillBucketHandler();
       break;
 
-    case document.getElementById('hotkeyPencil').value:
+    case hotkeyPencil.value:
       pencilHandler();
       break;
 
-    case document.getElementById('hotkeyEaser').value:
+    case hotkeyEraser.value:
       eraserHandler();
       break;
 
-    case document.getElementById('hotkeyChooseColor').value:
+    case hotkeyChooseColor.value:
       chooseColorHandler();
       break;
 
-    case 'a':
+    case hotkeyAddFrame.value:
       addFrame();
       break;
 
-    case document.getElementById('hotkeyFullScreen').value:
+    case hotkeyFullScreen.value:
       getFullScreen();
       break;
 
-    case document.getElementById('hotkeyGif').value:
+    case hotkeyGif.value:
       getGif();
       break;
 
-    case document.getElementById('hotkeySave').value:
+    case hotkeySave.value:
       saveBuffer();
       break;
 
-    case document.getElementById('hotkeyLoad').value:
+    case hotkeyLoad.value:
       loadBuffer();
       break;
 
-    case '1':
+    case hotkeyRes1.value:
       document.getElementById('res32').click();
       break;
 
-    case '2':
+    case hotkeyRes2.value:
       document.getElementById('res64').click();
       break;
 
-    case '3':
+    case hotkeyRes3.value:
       document.getElementById('res128').click();
       break;
 
@@ -100,8 +116,27 @@ function removeHidden() {
 hotKeyMenu.addEventListener('click', removeHidden);
 
 
+// ================================================================ set titles for hints
+function hintsInit() {
+  document.getElementById('fillBucket').setAttribute('title', `${hotkeyFillBucket.value}`);
+  document.getElementById('pencil').setAttribute('title', `${hotkeyPencil.value}`);
+  document.getElementById('eraser').setAttribute('title', `${hotkeyEraser.value}`);
+  document.getElementById('chooseColor').setAttribute('title', `${hotkeyChooseColor.value}`);
+  document.getElementById('Save').setAttribute('title', `${hotkeySave.value}`);
+  document.getElementById('Load').setAttribute('title', `${hotkeyLoad.value}`);
+  document.getElementById('addNewFrame').setAttribute('title', `${hotkeyAddFrame.value}`);
+  document.getElementById('res32').setAttribute('title', `${hotkeyRes1.value}`);
+  document.getElementById('res64').setAttribute('title', `${hotkeyRes2.value}`);
+  document.getElementById('res128').setAttribute('title', `${hotkeyRes3.value}`);
+  document.getElementById('fullScreen').setAttribute('title', `${hotkeyFullScreen.value}`);
+  document.getElementById('saveGif').setAttribute('title', `${hotkeyGif.value}`);
+}
+hintsInit();
+// ================================================================
+
 const close = document.getElementById('close');
 function addHidden() {
+  hintsInit();
   document.getElementById('hotkeysContainer').classList.add('hidden');
   document.addEventListener('keydown', keysActivation);
 }
